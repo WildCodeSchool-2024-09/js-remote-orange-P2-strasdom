d584d8307456cdec87975b127b18a18dd3432f43
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-11-12 18:11:49 +0100

    adding buttons on services

--- client/src/components/serviceModule/cardService/CardService.tsx
+++ client/src/components/serviceModule/cardService/CardService.tsx
@@ -1,4 +1,6 @@
-// import React from "react";
+import type React from "react";
+import { useState } from "react";
+
 import "./cardservice.css";
 
 interface Service {
@@ -13,7 +15,25 @@ type Props = {
   services: Service[];
 };
 
+const basket: Service[] = [];
+
 const CardService: React.FC<Props> = ({ services }) => {
+  const [numberOfHours, setNumberOfHours] = useState<number>(0);
+
+  const handleAddHour = () => {
+    setNumberOfHours(numberOfHours + 1);
+  };
+
+  const handleRemoveHour = () => {
+    if (numberOfHours > 0) {
+      setNumberOfHours(numberOfHours - 1);
+    }
+  };
+
+  const handleAddToBasket = (service: Service) => {
+    basket.push(service);
+  };
+
   return (
     <div>
       {services.map((service) => (
@@ -22,6 +42,31 @@ const CardService: React.FC<Props> = ({ services }) => {
           <p>Description: {service.description}</p>
           <p>Tarif: {service.tarif_horaire} €/h</p>
           <p>Disponibilité: {service.disponibilite ? "Oui" : "Non"}</p>
+          <p>Nombre d'heures: {numberOfHours}</p>
+          <button
+            type="button"
+            className="button"
+            id="AddingButton"
+            onClick={handleAddHour}
+          >
+            Ajouter une heure
+          </button>
+          <button
+            type="button"
+            className="button"
+            id="DeleteButton"
+            onClick={handleRemoveHour}
+          >
+            Retirer une heure
+          </button>
+          <button
+            type="button"
+            className="button"
+            id="AddToBasket"
+            onClick={() => handleAddToBasket(service)}
+          >
+            Ajouter au panier
+          </button>
         </div>
       ))}
     </div>
