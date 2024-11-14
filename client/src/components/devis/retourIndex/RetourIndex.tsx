08ad48492bcda2a296ba83db29b6fef56c7ee855
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-11-14 21:15:05 +0100

    MaJ CSS Etape 4

--- client/src/components/devis/retourIndex/RetourIndex.tsx
+++ client/src/components/devis/retourIndex/RetourIndex.tsx
@@ -25,37 +25,39 @@ const RetourIndex = ({
   };
 
   return (
-    <div>
-      <h1>Confirmation</h1>
-      <h2>Merci pour votre confiance, voici le devis imprimable</h2>
-      <div>
-        <h3>Vos informations</h3>
-        <p>{userInfo}</p>
+    <div className="ConfirmationModule">
+      <div className="card">
+        <h1>Confirmation</h1>
+        <h2>Merci pour votre confiance, voici le devis imprimable</h2>
+        <div>
+          <h3>Vos informations</h3>
+          <p>{userInfo}</p>
+        </div>
+        <div>
+          <h3>Services sélectionnés</h3>
+          <ul>
+            {selectedServices.map(
+              (
+                service, // Afficher les services sélectionnés
+              ) => (
+                <li key={service.id}>
+                  {service.nom} - {service.tarif_horaire}€/h
+                </li>
+              ),
+            )}
+          </ul>
+        </div>
+        <div>
+          <h3>Prix total à la semaine</h3>
+          <p>{totalWeeklyPrice}€</p>
+        </div>
+        <button type="button" id="button" onClick={handlePrint}>
+          Imprimer le devis
+        </button>
+        <button type="button" id="button" onClick={handleReturnToIndex}>
+          Retour à l'index
+        </button>
       </div>
-      <div>
-        <h3>Services sélectionnés</h3>
-        <ul>
-          {selectedServices.map(
-            (
-              service, // Afficher les services sélectionnés
-            ) => (
-              <li key={service.id}>
-                {service.nom} - {service.tarif_horaire}€/h
-              </li>
-            ),
-          )}
-        </ul>
-      </div>
-      <div>
-        <h3>Prix total à la semaine</h3>
-        <p>{totalWeeklyPrice}€</p>
-      </div>
-      <button type="button" onClick={handlePrint}>
-        Imprimer le devis
-      </button>
-      <button type="button" onClick={handleReturnToIndex}>
-        Retour à l'index
-      </button>
     </div>
   );
 };
