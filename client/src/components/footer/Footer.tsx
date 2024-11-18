6552caae61f3f0e1b625264b0bb55b8838a91b17
Qiu-Nick <nicolas.theaux.peria@gmail.com>
2024-11-18 13:18:34 +0100

    création du cta to bo sur footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,28 +1,54 @@
+import { useState } from "react";
 import "./footer.css";
 
 function Footer() {
-  const navigateToBackOffice = () => {
-    const password = prompt("Please enter the password:");
+  const [showPasswordDialog, setShowPasswordDialog] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordSubmit = () => {
     if (password === "Goûter") {
       window.location.href = "/backoffice"; // Navigate to BackOffice tab
     } else {
-      alert("C'est pas l'heure");
+      alert("La suite au prochain épisode...");
     }
+    setShowPasswordDialog(false);
+    setPassword("");
+  };
+
+  const navigateToBackOffice = () => {
+    setShowPasswordDialog(true);
   };
 
   return (
     <div className="footer-container">
-      <div className="footer-content">
-        <div className="footer-content-left">
-          <h1>Footer</h1>
+      {showPasswordDialog && (
+        <div className="password-dialog">
+          <div className="password-dialog-content">
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Please enter the password"
+            />
+            <button type="button" onClick={handlePasswordSubmit}>
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="footer-content-right">
-          <h1>Footer</h1>
-          <button type="button" onClick={navigateToBackOffice}>
-            Go to BackOffice
-          </button>
-        </div>
-      </div>
+      )}
+      <button type="button" id="button" onClick={navigateToBackOffice}>
+        Go to BackOffice
+      </button>
+      <button type="button" id="button">
+        About Us
+      </button>
+      <button type="button" id="button">
+        Mentions Légales
+      </button>
     </div>
   );
 }
