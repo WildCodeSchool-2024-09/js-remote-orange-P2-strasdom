e8a071f1b35df2de86bf6b0cb4698ff80b7f8935
Qiu-Nick <nicolas.theaux.peria@gmail.com>
2024-11-24 19:39:14 +0100

    css added to BO

--- client/src/pages/backOffice/tableauReservations/TableauReservations.tsx
+++ client/src/pages/backOffice/tableauReservations/TableauReservations.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useReservation } from "../../../context/ReservationContext";
+import "./TableauReservations.css";
 
 const TableauReservations = () => {
   const { reservations } = useReservation();
